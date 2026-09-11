0cf4f37c8562d405d1a42f0ea51e633b8593e85e
Bhavana <bhavanakonda1905@gmail.com>
2026-09-11 19:11:49 +0530

    Update even odd with a loop

--- even_odd.py
+++ even_odd.py
@@ -1,5 +1,7 @@
-num=int(input("Enter a number: "))
-if num%2==0:
-    print("Even number")
-else:
-    print("Odd number")
+for i in range(5):
+    num = int(input("Enter a number: "))
+
+    if num % 2 == 0:
+        print("Even number")
+    else:
+        print("Odd number")
